3266fde265672277ca3c4527dfce2acf8db3d010
reinvantveer <rein.van.t.veer@geodan.nl>
2017-10-18 14:25:14 +0000

    early stopping, archive

--- model/random_univariate_gaussian.py
+++ model/random_univariate_gaussian.py
@@ -1,37 +1,46 @@
 from datetime import datetime
 import numpy as np
+import os
 from keras import Input
-from keras.callbacks import TensorBoard
+from keras.callbacks import TensorBoard, EarlyStopping
 from keras.engine import Model
 from keras.layers import LSTM, TimeDistributed, Dense
 from keras.optimizers import Adam
+from shutil import copyfile
 
 from topoml_util.ConsoleLogger import DecypherAll
 from topoml_util.geom_loss import univariate_gaussian_loss
 
 TIMESTAMP = str(datetime.now()).replace(':', '.')
+SCRIPT_NAME = os.path.basename(__file__)
 EPOCHS = 200
 BATCH_SIZE = 100
 TRAINING_SIZE = 50000
 TRAIN_VALIDATE_SPLIT = 0.2
-tb_callback = TensorBoard(log_dir='./tensorboard_log/' + TIMESTAMP, histogram_freq=1, write_graph=True)
+
+# Archive the configuration
+copyfile(__file__, 'configs/' + TIMESTAMP + ' ' + SCRIPT_NAME)
 
 univariate = np.random.randint(low=1, high=20, size=(TRAINING_SIZE, 1, 1))
 univariate = np.append(univariate, np.zeros(shape=(TRAINING_SIZE, 1, 1)), axis=2)
-(_, max_points, GEO_VECTOR_LEN) = univariate.shape
+(_, max_points, SEQ_LEN) = univariate.shape
 
-inputs = Input(name='Input', shape=(max_points, GEO_VECTOR_LEN))
-model = LSTM(GEO_VECTOR_LEN, return_sequences=True)(inputs)
-model = TimeDistributed(Dense(GEO_VECTOR_LEN))(model)
+inputs = Input(name='Input', shape=(max_points, SEQ_LEN))
+model = LSTM(SEQ_LEN, return_sequences=True)(inputs)
+model = TimeDistributed(Dense(SEQ_LEN))(model)
 model = Model(inputs, model)
 model.compile(loss=univariate_gaussian_loss, optimizer=Adam(lr=0.001))
 model.summary()
 
-my_callback = DecypherAll(lambda x: str(x))
+callbacks = [
+    DecypherAll(lambda x: str(x)),
+    TensorBoard(log_dir='./tensorboard_log/' + TIMESTAMP + ' ' + SCRIPT_NAME, write_graph=False),
+    EarlyStopping(patience=40, min_delta=1e-3)
+]
 
 model.fit(x=univariate,
           y=univariate,
           epochs=EPOCHS,
           batch_size=BATCH_SIZE,
           validation_split=TRAIN_VALIDATE_SPLIT,
-          callbacks=[my_callback, tb_callback])
+          callbacks=callbacks)

f304c0d67aa43f5f027798405ce28f37b6560faf
reinvantveer <rein.van.t.veer@geodan.nl>
2017-10-18 12:45:53 +0000

    build script, test version

--- script/jenkins-build-script.sh
+++ script/jenkins-build-script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-CHANGED_FILES=`git diff --stat --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT`
-for FILE in ${CHANGED_FILES}
+CHANGED_MODEL_FILES=`git diff --stat --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | grep model`
+for FILE in ${CHANGED_MODEL_FILES}
 do
 	echo ${FILE}
 done

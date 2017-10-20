bd9054d7e88f9c4313e4f01ce8462ff4e778735c
reinvantveer <rein.van.t.veer@geodan.nl>
2017-10-20 11:24:34 +0000

    refactor

--- model/random_univariate_gaussian.py
+++ model/random_univariate_gaussian.py
@@ -1,5 +1,3 @@
-VERSION = "0.0.2"
-
 from datetime import datetime
 import numpy as np
 import os
@@ -14,6 +12,7 @@ from topoml_util.ConsoleLogger import DecypherAll
 from topoml_util.gaussian_loss import univariate_gaussian_loss
 from topoml_util.slack_send import notify
 
+SCRIPT_VERSION = "0.0.3"
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SCRIPT_NAME = os.path.basename(__file__)
 EPOCHS = 200
@@ -28,15 +27,15 @@ univariate = np.random.randint(low=1, high=20, size=(TRAINING_SIZE, 1, 1))
 (_, max_points, SEQ_LEN) = univariate.shape
 
 inputs = Input(name='Input', shape=(max_points, SEQ_LEN))
-model = LSTM(SEQ_LEN, return_sequences=True)(inputs)
+model = LSTM(SEQ_LEN, activation='relu', return_sequences=True)(inputs)
 model = Dense(2)(model)
 model = Model(inputs, model)
 model.compile(loss=univariate_gaussian_loss, optimizer=Adam(lr=0.001))
 model.summary()
 
 callbacks = [
-    DecypherAll(lambda x: str(x)),
     TensorBoard(log_dir='./tensorboard_log/' + TIMESTAMP + ' ' + SCRIPT_NAME, write_graph=False),
+    DecypherAll(lambda x: str(x)),
     EarlyStopping(patience=40, min_delta=1e-4)
 ]
 

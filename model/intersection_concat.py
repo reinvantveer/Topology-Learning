dfc2e885ad1cfba2fbce5093ff2f3d85700c9279
reinvantveer <rein.van.t.veer@geodan.nl>
2017-10-21 12:45:50 +0200

    version bump

--- model/intersection_concat.py
+++ model/intersection_concat.py
@@ -14,7 +14,7 @@ from topoml_util.PyplotLogger import DecypherAll
 from topoml_util.geom_scaler import localized_normal, localized_mean
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = "0.0.2"
+SCRIPT_VERSION = "0.0.3"
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 PLOT_DIR = './plots/' + TIMESTAMP + ' ' + SCRIPT_NAME

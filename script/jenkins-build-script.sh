40fa60e14b444d843756e54f221da2edf96965f9
reinvantveer <rein.van.t.veer@geodan.nl>
2017-10-23 10:42:17 +0200

    exit on error, exclude util libs

--- script/jenkins-build-script.sh
+++ script/jenkins-build-script.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-CHANGED_MODEL_FILES=`git diff --stat --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | grep model`
+set -e
+CHANGED_MODEL_FILES=`git diff --stat --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | grep model | grep -v topoml_util`
 cd model
 for FILE in ${CHANGED_MODEL_FILES}
 do

57a31891b43136715606ab5199fdf079ae09154d
reinvantveer <rein.van.t.veer@geodan.nl>
2017-10-24 15:06:34 +0200

    filter python models

--- script/build-script.sh
+++ script/build-script.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 # Jenkins style
-# CHANGED_MODEL_FILES=`git diff --stat --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | grep model | grep -v topoml_util`
+# CHANGED_MODEL_FILES=`git diff --stat --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | grep model | grep py | grep -v topoml_util`
 
 # TeamCity style
-CHANGED_MODEL_FILES="$(cat $1 | cut -d \: -f 1)"
+CHANGED_MODEL_FILES="$(cat $1 | cut -d \: -f 1 | grep model | grep py | grep -v topoml_util)"
 
-echo $CHANGED_MODEL_FILES
+echo ${CHANGED_MODEL_FILES}
 
 cd model
 for FILE in ${CHANGED_MODEL_FILES}

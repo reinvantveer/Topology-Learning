7ab3ca25ade206ed92dbc218582cc22abcaf9a62
reinvantveer <rein.van.t.veer@geodan.nl>
2017-10-24 14:44:07 +0200

    TeamCity style change list

--- script/build-script.sh
+++ script/build-script.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
-set -e
-CHANGED_MODEL_FILES=`git diff --stat --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | grep model | grep -v topoml_util`
+set -ex
+# Jenkins style
+# CHANGED_MODEL_FILES=`git diff --stat --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | grep model | grep -v topoml_util`
+
+# TeamCity style
+CHANGED_MODEL_FILES=`cat %system.teamcity.build.changedFiles.file% | cut -d \: -f 1`
+
+echo ${CHANGED_MODEL_FILES}
+
 cd model
 for FILE in ${CHANGED_MODEL_FILES}
 do

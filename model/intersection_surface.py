6b46f39ce7259b9eec6fabb665495a16caf2be0f
reinvantveer <rein.van.t.veer@geodan.nl>
2017-10-27 15:33:36 +0200

    tune

--- model/intersection_surface.py
+++ model/intersection_surface.py
@@ -12,7 +12,7 @@ from topoml_util.gaussian_loss import univariate_gaussian_loss
 from topoml_util.geom_scaler import localized_normal, localized_mean
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = "0.0.9"
+SCRIPT_VERSION = "0.0.10"
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + TIMESTAMP
@@ -21,7 +21,7 @@ BATCH_SIZE = 1024
 TRAIN_VALIDATE_SPLIT = 0.1
 LSTM_UNITS = 256
 DENSE_UNITS = 64
-REPEAT_HIDDEN = 4
+REPEAT_HIDDEN = 2
 EPOCHS = 800
 OPTIMIZER = Adam(lr=1e-3)
 

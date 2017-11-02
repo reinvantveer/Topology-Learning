1871820e8847c817304067c87661644078f42063
reinvantveer <rein.van.t.veer@geodan.nl>
2017-11-02 15:25:06 +0100

    one lstm layer, plot error distribution

--- model/geom_distance.py
+++ model/geom_distance.py
@@ -11,11 +11,13 @@ from topoml_util.ConsoleLogger import DecypherAll
 from topoml_util.gaussian_loss import univariate_gaussian_loss
 from topoml_util.geom_scaler import localized_normal, localized_mean
 from topoml_util.slack_send import notify
+from matplotlib import pyplot as plt
 
-SCRIPT_VERSION = "0.0.5"
+SCRIPT_VERSION = "0.0.6"
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + TIMESTAMP
+PLOT_DIR = 'plots/' + SIGNATURE
 DATA_FILE = '../files/geodata_vectorized.npz'
 BATCH_SIZE = 512
 TRAIN_VALIDATE_SPLIT = 0.1
@@ -37,8 +39,6 @@ inputs = Input(name='Input', shape=(max_points, GEO_VECTOR_LEN))
 model = LSTM(LATENT_SIZE, activation='relu', return_sequences=True)(inputs)
 model = TimeDistributed(Dense(32))(model)
 model = LeakyReLU()(model)
-model = LSTM(LATENT_SIZE, activation='relu', return_sequences=True)(model)
-model = TimeDistributed(Dense(32))(model)
 model = Flatten()(model)
 model = Dense(2)(model)
 model = Model(inputs, model)
@@ -53,7 +53,6 @@ callbacks = [
     EarlyStopping(patience=40, min_delta=0.001)
 ]
 
-
 history = model.fit(x=training_vectors,
                     y=target_vectors,
                     epochs=EPOCHS,
@@ -63,24 +62,13 @@ history = model.fit(x=training_vectors,
 
 val_set_start = -round(data_points * TRAIN_VALIDATE_SPLIT)
 prediction = model.predict(training_vectors[val_set_start:])
-
-intersecting_target = []
-intersecting_prediction = []
-non_intersecting_target = []
-non_intersecting_prediction = []
-
-for index, _ in enumerate(target_vectors[val_set_start:]):
-    if target_vectors[index, 0] == 0.:
-        intersecting_target.append(target_vectors[index])
-        intersecting_prediction.append(prediction[index])
-    else:
-        non_intersecting_target.append(target_vectors[index])
-        non_intersecting_prediction.append(prediction[index])
-
-intersecting_error = np.abs(np.array(intersecting_prediction)[:, 0] - np.array(intersecting_target)[:, 0])
-non_intersecting_error = np.abs(np.array(non_intersecting_prediction)[:, 0] - np.array(non_intersecting_target)[:, 0])
-print('Intersecting error factor:', np.mean(intersecting_error))
-print('Non-intersecting error factor:', np.mean(non_intersecting_error))
+error = prediction[:, 0] - target_vectors[val_set_start:, 0]
+fig, ax = plt.subplots()
+plt.text(0.01, 0.94, r'prediction error $\mu: $' + str(np.round(np.mean(error), 4)), transform=ax.transAxes)
+plt.text(0.01, 0.88, r'prediction error $\sigma: $' + str(np.round(np.std(error), 4)), transform=ax.transAxes)
+n, bins, patches = plt.hist(error, 50, facecolor='g', normed=True, alpha=0.75)
+os.makedirs(str(PLOT_DIR), exist_ok=True)
+plt.savefig(PLOT_DIR + '/plt_' + TIMESTAMP + '.png')
 
 notify(TIMESTAMP, SCRIPT_NAME, 'validation loss of ' + str(history['val_loss'][-1]))
 print(SCRIPT_NAME, 'finished successfully')

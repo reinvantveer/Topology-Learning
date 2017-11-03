a0e7c39922f37a79f274cb09db24731958c69db1
reinvantveer <rein.van.t.veer@geodan.nl>
2017-11-03 10:21:12 +0100

    tune

--- model/geom_distance.py
+++ model/geom_distance.py
@@ -13,7 +13,7 @@ from topoml_util.gaussian_loss import univariate_gaussian_loss
 from topoml_util.geom_scaler import localized_normal, localized_mean
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = "0.0.9"
+SCRIPT_VERSION = "0.0.10"
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + TIMESTAMP
@@ -39,6 +39,8 @@ inputs = Input(name='Input', shape=(max_points, GEO_VECTOR_LEN))
 model = LSTM(LATENT_SIZE, activation='relu', return_sequences=True)(inputs)
 model = TimeDistributed(Dense(32))(model)
 model = LeakyReLU()(model)
+model = LSTM(LATENT_SIZE, activation='relu', return_sequences=True)(model)
+model = TimeDistributed(Dense(32))(model)
 model = LSTM(LATENT_SIZE, activation='relu')(model)
 model = Dense(2)(model)
 model = Model(inputs, model)
